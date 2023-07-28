26dc9854325cae4031cfabf111ebfd4a6aaf84af
nastoika <nastya.gorlachyova@mail.ru@>
2023-07-28 21:45:19 +0300

    fix bugs with links CSS

--- src/components/InfoItem/InfoItem.tsx
+++ src/components/InfoItem/InfoItem.tsx
@@ -1,29 +1,34 @@
 import styles from './InfoItem.module.scss';
 
- export interface InfoItemProps {
+export interface InfoItemProps {
   icon: React.ReactNode,
   text?: string | null,
   isLink?: boolean,
 }
 
-export const InfoItem = ({icon, text, isLink}: InfoItemProps) => {
-  const currentText = text || 'Not available';
-  let currentHref : string = '';
+export const InfoItem = ({ icon, isLink, text}: InfoItemProps) => {
+  const currentText = text || 'Not Available';
+  let currentHref = '';
 
   if (isLink) {
-    currentHref = text && text.startsWith('http') ? text  : `https://${text}`
+    currentHref = text && text.startsWith('http') ? text : `https://${text}`;
   }
 
-  return(
-  <div className={`${styles.infoItem}${text ? '' :  `${styles.empty}`}`}>
-    {icon}
-    <div>
-      {isLink && text ? (
-        <a href={currentHref} target="blank" rel="noreferrer" className={styles.link}>
-          currentText
-        </a>
-      ) : currentText}
+  return (
+    <div className={`${styles.infoItem}${text ? '' : ` ${styles.empty}`}`}>
+      {icon}
+      <div>
+        {isLink && text ? (
+          <a
+            href={currentHref}
+            target="_blank"
+            rel="noreferrer"
+            className={styles.link}
+          >
+            {currentText}
+          </a>
+        ): currentText}
+      </div>
     </div>
-  </div>
-);
+  );
 };
